8f9eb97a2e8f2f48292229a636e9b35e354d0e39
hcqq <1462353231@qq.com>
2019-06-21 21:15:42 +0800

    引入UI库的TabBar组件及其改造

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
-import React from 'react';
-// 引入button按钮组件
-import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import React,{ Component,Fragment } from 'react';
+// 引入 TabBar 标签栏
+import { TabBar } from 'antd-mobile';
+import "./styles/iconfont.css"
 
-function App() {
-  return (
-    <div className="App">
-      {/* 组件代码 */}
-      <Button>default</Button><WhiteSpace />
-      <Button disabled>default disabled</Button><WhiteSpace />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+      fullScreen: false,
+    };
+  }
 
-      <Button type="primary">primary</Button><WhiteSpace />
-      <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
-
-      <Button type="warning">warning</Button><WhiteSpace />
-      <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
-    </div>
-  );
+  render() {
+    return (
+      <Fragment>
+        <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+        >
+          <TabBar.Item
+            title="首页"
+            key="Home"
+            icon={<span className="iconfont icon-home" /> }
+            selectedIcon={<span className="iconfont icon-home" /> }
+            selected={this.state.selectedTab === 'blueTab'}
+            // badge={1}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+            data-seed="logId"
+          >
+            80
+          </TabBar.Item>
+          <TabBar.Item
+            icon={ <span className="iconfont icon-gouwuche" /> }
+            selectedIcon={ <span className="iconfont icon-gouwuche" />
+            }
+            title="购物车"
+            key="Cart"
+            badge={'1'}
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+            data-seed="logId"
+          >
+            334
+          </TabBar.Item>
+          <TabBar.Item
+            icon={ <span className="iconfont icon-weibiaoti2fuzhi12" /> }
+            selectedIcon={ <span className="iconfont icon-weibiaoti2fuzhi12" />
+            }
+            title="我的"
+            key="Mine"
+            // dot  // 点
+            selected={this.state.selectedTab === 'greenTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'greenTab',
+              });
+            }}
+          >
+            51
+          </TabBar.Item>
+          
+        </TabBar>
+        </div>
+    </Fragment>
+    );
+  }
 }
 
 export default App;
