d2b9cb1ba3b94571ee0f6f47948bae0d6aed43e0
hcqq <1462353231@qq.com>
2019-06-21 19:02:03 +0800

    测试使用ui组件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+// 引入button按钮组件
+import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
 
 function App() {
   return (
     <div className="App">
-      移动端项目
+      {/* 组件代码 */}
+      <Button>default</Button><WhiteSpace />
+      <Button disabled>default disabled</Button><WhiteSpace />
+
+      <Button type="primary">primary</Button><WhiteSpace />
+      <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
+
+      <Button type="warning">warning</Button><WhiteSpace />
+      <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
     </div>
   );
 }
