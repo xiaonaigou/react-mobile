37e61322c6b7a1ca13f87147b4c20fd5197b0497
hcqq <1462353231@qq.com>
2019-06-24 16:13:23 +0800

    商品详情-返回上一页

--- src/pages/GoodsDetail.js
+++ src/pages/GoodsDetail.js
@@ -1,10 +1,17 @@
 import React,{ Component,Fragment } from 'react';
+import { NavBar, Icon } from 'antd-mobile';
 
 class GoodsDetail extends Component {
  render(){
      return(
          <Fragment>
-             GoodsDetail
+             <div>
+            <NavBar
+            mode="dark"
+            icon={<Icon type="left" />}
+            onLeftClick={() => this.props.history.go(-1)}
+            >商品详情</NavBar>
+        </div>
          </Fragment>
      )
  }
